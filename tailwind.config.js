cb6cc21f20e46c2c79dd8992a515216dcaff1aab
Ruben Ramirez <rramirez110911@gmail.com>
2021-11-07 13:40:06 -0800

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      orange: {
+        DEFAULT: '#f79b26',
+      }
+    },
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
